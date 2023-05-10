ac6e268ccf04db213410b01b96ed60e8f1157cf8
Amirhosein <mr.jlm1370@gmail.com>
2023-05-10 13:34:08 -0500

    some bug fixes and login logic added

--- uplaw_contract/src/app.ts
+++ uplaw_contract/src/app.ts
@@ -1,36 +1,37 @@
+const express = require("express");
+const cors = require("cors");
 
-const express = require('express')
-import { join } from 'path';
-import  dotenv from 'dotenv'
+import { join } from "path";
+import dotenv from "dotenv";
 
 //setup mongodb
-import  db  from './config/mongo'
-db()
+import db from "./config/mongo";
+db();
 
 // main router app
-import mainRouter from './router/mainRouter'
-
+import mainRouter from "./router/mainRouter";
 
 // setup  environment variable
-dotenv.config({ path: join(__dirname,'../../uplaw_contract/.env') })
-export const PORT = process.env.PORT 
+dotenv.config({ path: join(__dirname, "../../uplaw_contract/.env") });
+export const PORT = process.env.PORT;
 
 //setup Express
-export const app = express()
+export const app = express();
 
-
+// 👇️ configure CORS
+app.use(cors());
 
 // middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // path for uploadImage employee
-app.use('/image',express.static(join(__dirname,'image')))
+app.use("/image", express.static(join(__dirname, "image")));
 
 // main router
-app.use(mainRouter)
+app.use(mainRouter);
 
 // SETUP Application
-app.listen(PORT, ()=> {
-    console.log(`running application on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`running application on port ${PORT}`);
+});
