a4cca904bb5914fcfe1a1f155f76f11e82d0b1ff
hadi dasti <hadidasti98@hotmail.com>
2023-08-02 20:42:26 +0330

    delete router

--- uplaw_contract/build/router/api/v1/mainPage/mainPage.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-//import controller of mainPage
-const mainPage_1 = require("../../../../controller/mainPage/mainPage");
-// create mainPage Router
-router.get('/viewContract', mainPage_1.viewContractPage);
-exports.default = router;
